0b5b514ef26fa680d5a72e5755f6871658f59cf3
AlSidorenko <alek.sidorenko1979@gmail.com>
2019-02-24 20:46:35 +0200

    Add new regular expressions.

--- src/com/company/controller/RegexContainer.java
+++ src/com/company/controller/RegexContainer.java
@@ -8,6 +8,10 @@ public interface RegexContainer {
     String REGEX_NAME_UKR = "^[А-ЩЬЮЯҐІЇЄ][а-щьюяґіїє']{1,20}$";
     // Latin name
     String REGEX_NAME_LAT = "^[A-Z][a-z]{1,20}$";
+    /**Surname ukr.*/
+    String REGEX_SURNAME_URK = "^[А-ЩЬЮЯҐІЇЄ][а-щьюяґіїє']{1,20}$";
+    /**Surname lat.*/
+    String REGEX_SURNAME_LAT = "^[A-Z][a-z]{1,20}$";
     // login
     String REGEX_LOGIN = "^[A-Za-z0-9_-]{8,20}$";
     /**NickName.*/
